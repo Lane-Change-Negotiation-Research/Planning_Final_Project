17e2a451dce70d32096485c5e05790f791e0fb0a
Justin Morris <jpmorris5@gmail.com>
2020-11-29 22:01:52 -0500

    First attempt at circle-based collision checking.

--- lane_change/util.py
+++ lane_change/util.py
@@ -8,7 +8,7 @@ import sys
 import time
 
 try:
-    sys.path.append(glob.glob('../carla/dist/carla-*%d.%d-%s.egg' % (
+    sys.path.append(glob.glob('/opt/CARLA_0.9.9.4/PythonAPI/carla/dist/carla-*%d.%d-%s.egg' % (
         sys.version_info.major,
         sys.version_info.minor,
         'win-amd64' if os.name == 'nt' else 'linux-x86_64'))[0])
